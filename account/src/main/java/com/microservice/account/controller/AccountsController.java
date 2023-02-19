b1d79984ff64d419722a30174819d412347e5e78
naresh546nk <91820009+naresh546nk@users.noreply.github.com>
2023-02-19 20:11:32 +0530

    added rsiliancej4

--- account/src/main/java/com/microservice/account/controller/AccountsController.java
+++ account/src/main/java/com/microservice/account/controller/AccountsController.java
@@ -7,6 +7,8 @@ import com.microservice.account.config.AccountsConfig;
 import com.microservice.account.model.*;
 import com.microservice.account.repository.AccountsRepository;
 import com.microservice.account.service.CustomerServices;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -41,8 +43,8 @@ public class AccountsController {
 	}
 
 	@PostMapping("/customerDetails")
+	@CircuitBreaker(name = "customerDetailsCircuit", fallbackMethod = "customerDetialsCircuitFallback")
 	public CustomerDetials getCustomerDetails(@RequestBody Customer customer){
-
 		Accounts accountDetails = getAccountDetails(customer);
 		List<Loans> loansList = customerServices.getAllLones(customer).collectList().block();
 		List<Cards> cardsList = customerServices.getAllCards(customer).collectList().block();
@@ -51,10 +53,27 @@ public class AccountsController {
 		customerDetials.setAccounts(accountDetails);
 		customerDetials.setCards(cardsList);
 		customerDetials.setLoans(loansList);
+		return  customerDetials;
+	}
+
+	private  CustomerDetials customerDetialsCircuitFallback(Customer customer,Throwable throwable){
+		CustomerDetials customerDetials=new CustomerDetials();
+		Accounts accountDetails = getAccountDetails(customer);
+		customerDetials.setAccounts(accountDetails);
+		try{
+			List<Loans> loansList = customerServices.getAllLones(customer).collectList().block();
+			customerDetials.setLoans(loansList);
+		}catch (Exception e){
+			System.out.println(e.getMessage());
+		}
+		try{
+			List<Cards> cardsList = customerServices.getAllCards(customer).collectList().block();
+			customerDetials.setCards(cardsList);
+		}catch (Exception e){
+			System.out.println(e.getMessage());
+		}
 
 		return  customerDetials;
-
-
 	}
 
 	@GetMapping("/properties")
@@ -66,6 +85,17 @@ public class AccountsController {
 	@GetMapping("/")
 	public List<Accounts> getAllAccounts(){
 		return  accountsRepository.findAll();
+	}
+
+
+	@GetMapping("/hello")
+	@RateLimiter(name = "helloRateLimiter",fallbackMethod ="helloRateLimiterFallback" )
+	public String getHello(){
+		return  "Hi All, This is the api to test RateLimiter you can only call this api twice in  20 second";
+	}
+
+	private  String helloRateLimiterFallback(Throwable throwable){
+		return "You can not call this api more then twice in 20 second due to high cpu utilization";
 	}
 
 }
